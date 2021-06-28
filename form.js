5a2fbb83a01d7f61db69cba3e096dcb0d2036e5d
shikhar0512L <74178918+shikhar0512L@users.noreply.github.com>
2021-06-28 09:46:11 +0530

    Add files via upload
    Kindly write mouseEvent in form.js

---
+++ form.js
@@ -0,0 +1,16 @@
+console.log('Welcome to forms section');
+
+const alert = document.getElementById('alert');
+const submit = document.getElementById('submit');
+
+
+submit.addEventListener('click', func);
+
+function func(){
+console.log('Function is running');    
+}
+
+// function display() {
+//     console.log(localStorage);
+//     return true;
+// }
---
+++ main.js
@@ -0,0 +1,17 @@
+console.log(`Welcome to our website`);
+var date = new Date();
+console.log(date);
+
+const link = document.getElementsByClassName(`link`);
+const Rajasthan = document.getElementsByClassName(` Rajasthan`);
+const Kashmir = document.getElementsByClassName(` Kashmir`);
+
+console.log(Rajasthan);
+console.log(link);
+console.log(Kashmir);
+
+
+
+// for (let i = 0; i <200; i++) {
+//     console.log(i);
+// };
